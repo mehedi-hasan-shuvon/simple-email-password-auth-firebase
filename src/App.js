1bd707d4f48fefd597f5ca549e9ec10550fbacfa
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-08 01:43:21 +0600

    created a basic from and imported react bootstrap

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import app from './firebase.init';
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 function App() {
+
+  const handelEmailBlur = (event) => {
+    console.log(event.target.value);
+  };
+  const handelPasswordBlur = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handelFormSubmit = (event) => {
+    console.log("form submitted");
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
-      <form action="">
-        <input type="text" />
-        <input type="password" />
+      <form onSubmit={handelFormSubmit}>
+        <input onBlur={handelEmailBlur} type="email" name="" id="" />
+        <br />
+        <input onBlur={handelPasswordBlur} type="password" name="" id="" />
+        <br />
+        <input type="submit" value="Login" />
       </form>
     </div>
   );
