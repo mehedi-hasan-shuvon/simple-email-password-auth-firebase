9cfda714c06596d27ea0738e2f5329ee793948af
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-08 02:35:19 +0600

    added special expression regex to check password validation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import { useState } from 'react';
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 function App() {
-
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -23,6 +24,20 @@ function App() {
   const handelFormSubmit = (event) => {
     // console.log("form submitted");
     // console.log(email, password);
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('password should contain at least one speical charecter');
+      return;
+    }
+    setValidated(true);
+    setError('');
+
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         const user = result.user;
@@ -46,10 +61,13 @@ function App() {
 
       <div className="registration w-50 mx-auto mt-5">
         <h2 className='text-primary'>Please Register Bro</h2>
-        <Form onSubmit={handelFormSubmit}>
+        <Form noValidate validated={validated} onSubmit={handelFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handelEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Control onBlur={handelEmailBlur} type="email" placeholder="Enter email" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -57,11 +75,15 @@ function App() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handelPasswordBlur} type="password" placeholder="Password" />
+            <Form.Control onBlur={handelPasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password more than  charecter.
+            </Form.Control.Feedback>
           </Form.Group>
           {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group> */}
+          <p className='text-danger'>{error}</p>
           <Button variant="primary" type="submit">
             Submit
           </Button>
